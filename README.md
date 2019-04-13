f601026abf40729dbb7f575e97bfaa63d70c07ee
ll9 <llwierer@web.de>
2019-04-13 10:56:15 +0200

    demo

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+Basic Project to show how to use custom components, state, clickhandler in react
+
+
+---
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/Person.js
+++ src/Person.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Person extends Component {
+  state = {
+    teststate: 22
+  };
+
+  clickHandler = () => {
+    alert("clicked");
+  };
+
+  increaseStateHandler = () => {
+    this.setState({ teststate: this.state.teststate + 1 });
+    this.state.teststate++;
+  };
+
   render() {
-      return (
-          <div>
-              <h1>Person Component</h1>
-              <h1>{this.props.name}</h1>
-          </div>
-      );
+    return (
+      <div>
+        <h1>Person Component</h1>
+        <h1>{this.props.name}</h1>
+        <button onClick={this.clickHandler}>React Click Handler</button>
+        <p>
+          My current stateproperty 'teststate' has the value{" "}
+          {this.state.teststate}
+        </p>
+        <button onClick={this.increaseStateHandler}>Increate State</button>
+      </div>
+    );
   }
 }
 
