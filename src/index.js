b8203c98dbcc99f0e6bf5691e4a3c903a4e4cafc
ll9 <llwierer@web.de>
2019-04-13 10:46:35 +0200

    created custom component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Person from './Person';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+    <div>
+        <Person name="Hans" />
+    </div>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
